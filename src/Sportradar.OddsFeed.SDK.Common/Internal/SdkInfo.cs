73f6696b2fe5b7a1699b8745c800b8e4fe6a66c7
David Hrovat <d.hrovat@sportradar.com>
2019-12-17 13:10:37 +0100

    Fix: returning market decimal value in culture invariant format

--- src/Sportradar.OddsFeed.SDK.Common/Internal/SdkInfo.cs
+++ src/Sportradar.OddsFeed.SDK.Common/Internal/SdkInfo.cs
@@ -188,11 +188,11 @@ namespace Sportradar.OddsFeed.SDK.Common.Internal
             string result;
             if (input > 0)
             {
-                result = $"+{input}";
+                result = $"+{input.ToString(CultureInfo.InvariantCulture)}";
             }
             else if (input < 0)
             {
-                result = $"{input}";
+                result = $"{input.ToString(CultureInfo.InvariantCulture)}";
             }
             else
             {
