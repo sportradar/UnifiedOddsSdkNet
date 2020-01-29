664b4b77836b751b03118c52eb2d43cabd5cc063
Srđan Tot <s.tot@sportradar.com>
2020-01-29 14:54:08 +0100

    Added State to the Venue (extended with IVenueV1) (FEEDSDK-1354)

---
+++ src/Sportradar.OddsFeed.SDK.Entities.REST/IVenueV1.cs
@@ -0,0 +1,16 @@
+﻿/*
+* Copyright (C) Sportradar AG. See LICENSE for full license governing this code
+*/
+namespace Sportradar.OddsFeed.SDK.Entities.REST
+{
+    /// <summary>
+    /// Defines a contract implemented by classes representing a sport event venue
+    /// </summary>
+    public interface IVenueV1 : IVenue
+    {
+        /// <summary>
+        /// Gets a state of the venue represented by current <see cref="IVenue" /> instance
+        /// </summary>
+        string State { get; }
+    }
+}
--- src/Sportradar.OddsFeed.SDK.Entities.REST/Internal/EntitiesImpl/Venue.cs
+++ src/Sportradar.OddsFeed.SDK.Entities.REST/Internal/EntitiesImpl/Venue.cs
@@ -14,7 +14,7 @@ namespace Sportradar.OddsFeed.SDK.Entities.REST.Internal.EntitiesImpl
     /// <summary>
     /// Represents a sport event venue
     /// </summary>
-    internal class Venue : EntityPrinter, IVenue
+    internal class Venue : EntityPrinter, IVenueV1
     {
         /// <summary>
         /// Gets a <see cref="URN"/> uniquely identifying the current <see cref="IVenue" /> instance
@@ -55,6 +55,12 @@ namespace Sportradar.OddsFeed.SDK.Entities.REST.Internal.EntitiesImpl
         public string CountryCode { get; }
 
         /// <summary>
+        /// Gets a state of the venue represented by current <see cref="IVenue" /> instance
+        /// </summary>
+        /// <value>The state.</value>
+        public string State { get; }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="Venue"/> class.
         /// </summary>
         /// <param name="ci">A <see cref="VenueCI"/> used to create new instance</param>
@@ -77,6 +83,7 @@ namespace Sportradar.OddsFeed.SDK.Entities.REST.Internal.EntitiesImpl
             Countries = new ReadOnlyDictionary<CultureInfo, string>(
                 cultureList.Where(c => ci.GetCountry(c) != null).ToDictionary(c => c, ci.GetCountry));
             CountryCode = ci.CountryCode;
+            State = ci.State;
         }
 
         /// <summary>
@@ -112,7 +119,7 @@ namespace Sportradar.OddsFeed.SDK.Entities.REST.Internal.EntitiesImpl
             var cityNames = string.Join(", ", Cities.Select(x => x.Key.TwoLetterISOLanguageName + ":" + x.Value));
             var countryNames = string.Join(", ", Countries.Select(x => x.Key.TwoLetterISOLanguageName + ":" + x.Value));
 
-            return $"Id={Id}, Capacity={Capacity}, Coordinates={Coordinates}, Names=[{names}], Cities=[{cityNames}], Countries=[{countryNames}], CountryCode={CountryCode}";
+            return $"Id={Id}, Capacity={Capacity}, Coordinates={Coordinates}, Names=[{names}], Cities=[{cityNames}], Countries=[{countryNames}], CountryCode={CountryCode}, State={State}";
         }
 
         /// <summary>
