c1e829f1bb9dbde5cae7c8e345c21e44b41366e0
David Hrovat <d.hrovat@sportradar.com>
2021-03-09 15:27:36 +0100

    Added period_of_leader to the SportEventStatus.Properties Added StartTime, EndTime and AamsId to the IMarketMetaData (FEEDSDK-1604)

--- src/Sportradar.OddsFeed.SDK.Entities/IMarketMetadata.cs
+++ src/Sportradar.OddsFeed.SDK.Entities/IMarketMetadata.cs
@@ -1,6 +1,9 @@
 ﻿/*
 * Copyright (C) Sportradar AG. See LICENSE for full license governing this code
 */
+
+using System;
+
 namespace Sportradar.OddsFeed.SDK.Entities
 {
     /// <summary>
@@ -13,5 +16,41 @@ namespace Sportradar.OddsFeed.SDK.Entities
         /// </summary>
         /// <value>The next betstop</value>
         long? NextBetstop { get; }
+
+        /// <summary>
+        /// Gets the start time of the event (as epoch timestamp)
+        /// </summary>
+        /// <value>The start time</value>
+        long? StartTime { get; }
+
+        /// <summary>
+        /// Gets the end time of the event (as epoch timestamp)
+        /// </summary>
+        /// <value>The end time</value>
+        long? EndTime { get; }
+
+        /// <summary>
+        /// Gets date/time when to betstop the associated market. Typically used for outrights and typically is the start-time of the event the market refers to
+        /// </summary>
+        /// <value>The next betstop</value>
+        DateTime? NextBetstopDate { get; }
+
+        /// <summary>
+        /// Gets the start time of the event
+        /// </summary>
+        /// <value>The start time</value>
+        DateTime? StartTimeDate { get; }
+
+        /// <summary>
+        /// Gets the end time of the event
+        /// </summary>
+        /// <value>The end time</value>
+        DateTime? EndTimeDate { get; }
+
+        /// <summary>
+        /// Gets the Italian AAMS id for this outright
+        /// </summary>
+        /// <value>The Italian AAMS id for this outright</value>
+        long? AamsId { get; }
     }
 }
