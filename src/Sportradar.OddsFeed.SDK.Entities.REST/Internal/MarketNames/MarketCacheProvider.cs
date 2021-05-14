d52939a4c1e73f1d1667b9d094e8a5a73c99cc78
David Hrovat <d.hrovat@sportradar.com>
2021-05-14 16:19:56 +0200

    Fix: corrected which market description is returned for variant markets (FEEDSDK-1678)

--- src/Sportradar.OddsFeed.SDK.Entities.REST/Internal/MarketNames/MarketCacheProvider.cs
+++ src/Sportradar.OddsFeed.SDK.Entities.REST/Internal/MarketNames/MarketCacheProvider.cs
@@ -182,22 +182,7 @@ namespace Sportradar.OddsFeed.SDK.Entities.REST.Internal.MarketNames
                 _executionLog.Warn($"There was an error providing the explicit variant market description -> marketId:{marketId}, variantValue: {variantValue}, locales: [{langs}]", e);
             }
 
-            if (marketDescription != null && variantDescription != null)
-            {
-                if (marketDescription.Mappings.IsNullOrEmpty() && !variantDescription.Mappings.IsNullOrEmpty())
-                {
-                    ((MarketDescription) marketDescription).SetMappings(variantDescription.Mappings as IReadOnlyCollection<IMarketMappingData>);
-                    ((MarketDescription) marketDescription).SetFetchInfo("VariantCache", DateTime.Now);
-                }
-
-                if (marketDescription.Outcomes.IsNullOrEmpty() && !variantDescription.Outcomes.IsNullOrEmpty())
-                {
-                    ((MarketDescription) marketDescription).SetOutcomes(variantDescription.Outcomes as IReadOnlyCollection<IOutcomeDescription>);
-                    ((MarketDescription) marketDescription).SetFetchInfo("VariantCache", DateTime.Now);
-                }
-            }
-
-            return marketDescription ?? variantDescription;
+            return variantDescription ?? marketDescription;
         }
 
         private static bool IsMarketPlayerProps(IMarketDescription marketDescriptor)
