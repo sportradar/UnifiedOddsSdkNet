54374196e6fc4f6c53fe78b6f6330d893a04120f
David Hrovat <d.hrovat@sportradar.com>
2021-05-25 21:06:20 +0200

    Extended ITournament with GetScheduleAsync (extended with ITournamentV2) (FEEDSDK-1702)

---
+++ src/Sportradar.OddsFeed.SDK.Entities.REST/ITournamentV2.cs
@@ -0,0 +1,21 @@
+﻿/*
+* Copyright (C) Sportradar AG. See LICENSE for full license governing this code
+*/
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sportradar.OddsFeed.SDK.Entities.REST
+{
+    /// <summary>
+    /// Defines a contract implemented by classes providing information about tournament schedule
+    /// </summary>
+    public interface ITournamentV2 : ITournamentV1
+    {
+        /// <summary>
+        /// Gets the list of all <see cref="ICompetition"/> that belongs to the basic tournament schedule
+        /// </summary>
+        /// <returns>The list of all <see cref="ICompetition"/> that belongs to the basic tournament schedule</returns>
+        Task<IEnumerable<ISportEvent>> GetScheduleAsync();
+    }
+}
